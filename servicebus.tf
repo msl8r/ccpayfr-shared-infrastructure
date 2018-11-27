fa23ab3eab97248f89ab6f1ff4de2317eb13c103
Sateesh Mathangi <Sateesh.Mathangi@kcom.com>
2018-11-27 13:17:07 +0000

    fixed service bus config (#12)

--- servicebus.tf
+++ servicebus.tf
@@ -6,6 +6,8 @@ module "servicebus-namespace" {
   source              = "git@github.com:hmcts/terraform-module-servicebus-namespace.git"
   name                = "${var.product}-servicebus-${var.env}"
   location            = "${var.location}"
+  env                 = "${var.env}"
+  common_tags         = "${var.common_tags}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 }
 
