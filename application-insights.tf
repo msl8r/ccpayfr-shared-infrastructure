cdedbe6270d981b8f482b270ce20e8ea3323daae
Jalal ul Deen <jalal.din@gmail.com>
2019-01-07 13:12:13 +0000

    Create application-insights.tf

---
+++ application-insights.tf
@@ -0,0 +1,11 @@
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.product}-${var.env}"
+  location            = "${var.appinsights_location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  application_type    = "${var.application_type}"
+  tags                = "${var.common_tags}"
+}
+
+output "appInsightsInstrumentationKey" {
+  value = "${azurerm_application_insights.appinsights.instrumentation_key}"
+}
