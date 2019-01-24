26ec7aee82cca9c0a1e41b975b4b7c02b2d43710
Jalal ul Deen <jalal.din@gmail.com>
2019-01-24 08:40:04 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,3 +30,9 @@ variable "team_name" {
 variable "team_contact" {
   default = "#cc-payments-tech "
 }
+// as of now, UK South is unavailable for Application Insights
+variable "appinsights_location" {
+  type = "string"
+  default = "West Europe"
+  description = "Location for Application Insights"
+}
