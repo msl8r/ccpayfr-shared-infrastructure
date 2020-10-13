6bc0e15731fb3f9c377a22d585a858b238984bfa
amitchidrewar1301 <amit.chidrewar@hmcts.net>
2020-10-13 16:11:40 +0100

    PAY-3722: Changes for fees register MI

--- key-vault.tf
+++ key-vault.tf
@@ -26,11 +26,16 @@ module "feesregister-vault" {
   product_group_object_id = "56679aaa-b343-472a-bb46-58bbbfde9c3d"
   common_tags         = var.common_tags
   #aks migration
-  managed_identity_object_ids = ["${var.managed_identity_object_id}", "${module.ccpay-vault.managed_identity_objectid}"]
+  managed_identity_object_ids = ["${var.managed_identity_object_id}", "${data.azurerm_user_assigned_identity.ccpay-shared-identity.principal_id}"]
 }
 
 data "azurerm_key_vault" "ccpay_key_vault" {
   name                = module.ccpay-vault.key_vault_name
   resource_group_name = azurerm_resource_group.rg.name
+}
+
+data "azurerm_user_assigned_identity" "ccpay-shared-identity" {
+  name                = "${var.product}-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
 }
 
