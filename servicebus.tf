16bd12cc7f90073fe74d618e92cbffb93ab34e8e
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2021-09-13 15:11:58 +0100

    Create CCPAY cpo topic and subscription - Demo (#66)
    * https://tools.hmcts.net/jira/browse/PAY-5177
    
    * https://tools.hmcts.net/jira/browse/PAY-5177
    
    Co-authored-by: maharajan-jegan <maharajan.jegan@hmcts.net>

--- servicebus.tf
+++ servicebus.tf
@@ -19,6 +19,13 @@ module "topic" {
   resource_group_name   = azurerm_resource_group.rg.name
 }
 
+module "topic_cpo" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-topic"
+  name                  = "ccpay-cpo-Topic"
+  namespace_name        = module.servicebus-namespace.name
+  resource_group_name   = azurerm_resource_group.rg.name
+}
+
 module "queue" {
   source                = "git@github.com:hmcts/terraform-module-servicebus-queue"
   name                  = local.retry_queue
@@ -36,7 +43,15 @@ module "subscription" {
   forward_dead_lettered_messages_to = module.queue.name
 }
 
-
+module "subscription_cpo" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription"
+  name                  = local.subscription_name
+  namespace_name        = module.servicebus-namespace.name
+  topic_name            = module.topic_cpo.name
+  resource_group_name   = azurerm_resource_group.rg.name
+  max_delivery_count    = "1"
+  # forward_dead_lettered_messages_to = module.queue.name
+}
 
 resource "azurerm_key_vault_secret" "servicebus_primary_connection_string" {
   name      = "sb-primary-connection-string"
@@ -44,15 +59,21 @@ resource "azurerm_key_vault_secret" "servicebus_primary_connection_string" {
   key_vault_id = data.azurerm_key_vault.ccpay_key_vault.id
 }
 
+resource "azurerm_key_vault_secret" "cpo-topic-primary-send-listen-shared-access-key" {
+  name         = "cpo-topic-primary-send-listen-shared-access-key"
+  value        = module.topic_cpo.primary_send_and_listen_shared_access_key
+  key_vault_id = data.azurerm_key_vault.ccpay_key_vault.id
+}
+
 # primary connection string for send and listen operations
-output "sb_primary_send_and_listen_connection_string" {
-  value = module.servicebus-namespace.primary_send_and_listen_connection_string
-}
+# output "sb_primary_send_and_listen_connection_string" {
+#   value = module.servicebus-namespace.primary_send_and_listen_connection_string
+# }
 
-output "topic_primary_send_and_listen_connection_string" {
-  value = module.topic.primary_send_and_listen_connection_string
-}
+# output "topic_primary_send_and_listen_connection_string" {
+#   value = module.topic.primary_send_and_listen_connection_string
+# }
 
-output "psc_subscription_connection_string" {
-  value = "${module.topic.primary_send_and_listen_connection_string}/subscriptions/${local.subscription_name}"
-}
+# output "psc_subscription_connection_string" {
+#   value = "${module.topic.primary_send_and_listen_connection_string}/subscriptions/${local.subscription_name}"
+# }
